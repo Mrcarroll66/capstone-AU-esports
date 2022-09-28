f41d8e175e54471d3a5632e6a96eae4b8f5a1488
Michael Carroll <mrcarroll@anderson.edu>
2022-09-28 12:08:24 -0400

    import home

--- auesports/src/App.tsx
+++ auesports/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home  from "./pages/Home";
 
 const App: React.FC = () => {
     return <></>;
