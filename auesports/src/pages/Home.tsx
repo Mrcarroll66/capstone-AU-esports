e80b544dcf10e937c2ea226e4de6a5bc0e58b23c
Michael Carroll <mrcarroll@anderson.edu>
2022-09-28 12:28:08 -0400

    home template

--- auesports/src/pages/Home.tsx
+++ auesports/src/pages/Home.tsx
@@ -1,4 +1,22 @@
 const Home: React.FC = () => {
-    return <></>;
+    return (
+        <>
+            <div className="schedule"></div>
+            <div className="navBar"></div>
+            <div className="home">
+                <div className="News/Videos">
+                    <div className="NewsArticle"></div>
+                    <div className="Headline"></div>
+                    <div className="Recent Highlight"></div>
+                </div>
+                <div className="Teams">
+                    <div className="Team"></div>
+                    <div className="Team"></div>
+                    <div className="Team"></div>
+                </div>
+            </div>
+            <div className="Contact"></div>
+        </>
+    );
 };
 export default Home;
