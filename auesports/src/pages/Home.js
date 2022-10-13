40eed9391040e6ec5de19e7eeb38f1906b3ff6e3
Jacob <jwranahan@anderson.edu>
2022-10-13 16:06:18 -0400

    Added carousel to home page

--- auesports/src/pages/Home.js
+++ auesports/src/pages/Home.js
@@ -9,54 +9,71 @@ import ft from "../images/fortnite.png";
 import cod from "../images/callofduty.png";
 import ssmb from "../images/smash.png";
 import apex from "../images/apex.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
 
 function Home() {
     return (
         <>
-            <div>
-            <div className="newscontainer">
-                <a className="headline">News that's big</a>
-                <a className="newsbody">
-                    Filler text that is also the news body, a really long description to show how the text is going to
-                    react to going over
-                </a>
-                <a className="headline">apex news</a>
-                <a className="newsbody">Here is some more filler text</a>
-                <a className="headline">More news</a>
-                <a className="newsbody">Here is some more filler text</a>
-                <a className="headline">More news</a>
-                <a className="newsbody">Here is some more filler text</a>
+            <div className="newsContainer">
+                <Carousel>
+                    <Carousel.Item>
+                        <img className="newsimage" src={ow} alt="First slide" />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="newsimage" src={lol} alt="Second slide" />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="newsimage" src={apex} alt="Third slide" />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
-                <div className="team-container">
-                    <a href="https://playoverwatch.com/en-us/">
-                        <img src={ow} alt="Overwatch" className="owimg" />
-                    </a>
-                    <a href="https://www.leagueoflegends.com/en-us/">
-                        <img src={lol} alt="League of Legends" className="sqimg" />
-                    </a>
-                    <a href="https://www.rocketleague.com/">
-                        <img src={rl} alt="Rocket League" className="rlimg" />
-                    </a>
-                    <a href="https://playvalorant.com/en-us/">
-                        <img src={val} alt="Valorant" className="sqimg" />
-                    </a>
-                    <a href="https://www.epicgames.com/fortnite/en-US/home">
-                        <img src={ft} alt="Fortnite" className="sqimg" />
-                    </a>
-                </div>
-                <div className="team-container">
-                    <a href="https://www.callofduty.com/">
-                        <img src={cod} alt="Call of Duty" className="codimg" />
-                    </a>
-                    <a href="https://www.smashbros.com/en_US/">
-                        <img src={ssmb} alt="Super Smash Bros" className="sqimg" />
-                    </a>
-                    <a href="https://www.ea.com/games/apex-legends">
-                        <img src={apex} alt="Apex Legends" className="sqimg" />
-                    </a>
-                </div>
-            </div>
-           
+            <Container fluid>
+                <Row className="team-container">
+                    <Col>
+                        <img src={ow} alt="Overwatch" />
+                    </Col>
+                    <Col>
+                        <img src={lol} alt="League of Legends" />
+                    </Col>
+                    <Col>
+                        <img src={rl} alt="Rocket League" />
+                    </Col>
+                    <Col>
+                        <img src={val} alt="Valorant" />
+                    </Col>
+                    <Col>
+                        <img src={ft} alt="Fortnite" />
+                    </Col>
+                </Row>
+                <Row className="team-container">
+                    <Col>
+                        <img src={apex} alt="Apex Legends" />
+                    </Col>
+                    <Col>
+                        <img src={cod} alt="Call of Duty" />
+                    </Col>
+                    <Col>
+                        <img src={ssmb} alt="Super Smash Bros" />
+                    </Col>
+                </Row>
+            </Container>
             <footer className="contact">
                 <p>Email@email.com</p>
                 <p>(123) 456-789</p>
