f566863b1cac40890d4f73402f1408d2596835f0
jdspires00 <jdspires@anderson.edu>
2022-10-17 12:45:42 -0400

    Tiny fixes 10/17

--- auesports/src/pages/Home.js
+++ auesports/src/pages/Home.js
@@ -27,8 +27,9 @@ function Home () {
 				<nav id="topNav">
 					<ul className="navLinks" >
 						<li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
 						<li><a href="/teams">Teams</a></li>
-						<li><a href="/about">About</a></li>
+                        <li><a href="/news">News</a></li>
 						<li><a href="/contact">Contact</a></li>
 					</ul>
 				</nav>
