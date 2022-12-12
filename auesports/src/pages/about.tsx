ad641738f268343cae66a1d5150c339ec10125af
jdspires00 <jdspires@anderson.edu>
2022-12-12 18:10:57 -0500

    merge changes

--- auesports/src/pages/about.tsx
+++ auesports/src/pages/about.tsx
@@ -1,47 +1,12 @@
-import react from "react";
-import "./about.css";
-import ow from "../images/Overwatch_2.png";
-import lol from "../images/LOL.png";
-import rl from "../images/Rocket-League.png";
-import val from "../images/valorant.png";
-import ft from "../images/fortnite.png";
-import cod from "../images/callofduty.png";
-import ssmb from "../images/smash.png";
-import apex from "../images/apex.png";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import raven from "../images/raven.png";
-import Carousel from "react-bootstrap/Carousel";
-
-function About() {
+const About: React.FC = () => {
     return (
-        <>
-            <header className="webHeader">
-                <div className="headerTitles">
-                    <span className="headerTitleSm">React & Node</span>
-                    <span className="headerTitleLg">Blog</span>
-                </div>
-                <img className="ravenimg" src={raven} alt="Raven" />
-            </header>
-            <body>
-                <nav id="topNav">
-                    <ul className="navLinks">
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/teams">Teams</a>
-                        </li>
-                        <li>
-                            <a href="/about">About</a>
-                        </li>
-                        <li>
-                            <a href="/contact">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </body>
-            </>
-        );
-}
+        <div>
+            <h1>Anderson University Esports</h1>
+            <h3>Contact Us:</h3>
+            <p>Phone: 765-641-4321</p>
+            <p>Email: auravens@anderson.edu</p>
+            <p>Address: 1100 East 5th Street, Anderson, IN 46012</p>
+        </div>
+    );
+};
+export default About;
